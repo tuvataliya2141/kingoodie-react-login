52be6ef072c8dd12e4093d134318b0510f714586
Yagnesh <hadiyayagnesh@gmail.com>
2023-07-19 15:51:15 +0530

    fix sticky footer and header marque

--- src/component/StickyCart.js
+++ src/component/StickyCart.js
@@ -6,13 +6,13 @@ const StickyCart = () => {
   const { totalCount } = useAppContext();
   return (
     <div className="productList">
-      <h4>There are {totalCount.length} products in your cart.</h4>
+      <h4>There are <span>{totalCount.length}</span> products in your cart.</h4>
       <div className="footer-button">
         <Link to={"/Checkout"}>
-          <button className="btn btn-sm">CheckOut</button>
+          <button className="btn btn-xs">CheckOut</button>
         </Link>
         <Link to={"/Cart"}>
-          <button className="btn btn-sm">Go to cart</button>
+          <button className="btn btn-xs">Go to cart</button>
         </Link>
       </div>
     </div>
